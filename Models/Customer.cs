14f5550586da61f9c323734372ff71093ed6715b
Bailee Gerlich <bngerlich2@buffs.wtamu.edu>
2022-11-29 21:53:40 -0600

    EF Tables Push

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -17,5 +17,20 @@ namespace ProductCustomers.Models {
         [Required]
         public string cEmail {get; set;} = string.Empty;
 
+        public List<Order> Orders {get; set;} = default!; // Navigation property. Customer can have Many Orders
+
+    }
+    
+    public class Order{
+        public int productID {get; set;} // PK, FK 1 to Product Table
+        public Product Product {get; set;} = default!; //Navigation Property
+
+        public int customerID {get;set;} // PK, FK 2 to Product Table
+        public Customer Customer {get; set;} = default!; //Navigation Property
+        public int orderNum {get;set;} 
+        public DateTime orderDate {get; set;}
+
+        
+
     }
 }
--- Models/Products.cs
+++ Models/Products.cs
@@ -1,7 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 
 namespace ProductCustomers.Models {
-    public class BBProduct{
+    public class Product{
         public int productID {get; set;}
 
         [Display(Name ="Product Name")]
@@ -12,7 +12,7 @@ namespace ProductCustomers.Models {
         [Required]
         public double pPrice {get; set;}
 
-        
+        public List<Order> Orders {get; set;} = default!; //Navigation Property Product can be in many Orders
 
     }
 }
