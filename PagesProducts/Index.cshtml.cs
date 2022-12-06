0d98c1f859299554bcc18aba67c249760e217966
Bailee Gerlich <bngerlich2@buffs.wtamu.edu>
2022-12-05 21:54:45 -0600

    help Push

--- PagesProducts/Index.cshtml.cs
+++ PagesProducts/Index.cshtml.cs
@@ -19,6 +19,13 @@ namespace FinalProject_BaiBeauty.PagesProducts
         }
 
         public IList<Product> Product { get;set; } = default!;
+        //Search Items
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString {get;set;}
+       
+
+        [BindProperty(SupportsGet = true)]
+        public string ProductName { get; set; } = default!;
         
         //Paging support
         [BindProperty(SupportsGet = true)]
@@ -33,7 +40,11 @@ namespace FinalProject_BaiBeauty.PagesProducts
            
             if (_context.Products != null)
             {
-                 //Sorting query
+                //Search item query using LINQ
+                var ProductName = from p in _context.Products
+                    select p;
+
+                 //Sorting query using LINQ
                 var query = _context.Products.Select(p => p);
                 switch(CurrentSort){
                     case "first_asc":
@@ -41,6 +52,12 @@ namespace FinalProject_BaiBeauty.PagesProducts
                         break;
                     case "first_desc":
                         query = query.OrderByDescending(p=>p.pName);
+                        break;
+                    case "sec_asc":
+                        query = query.OrderBy(p=>p.pPrice);
+                        break;
+                    case "sec_desc":
+                        query = query.OrderByDescending(p=>p.pPrice);
                         break;
                 }
                 Product = await _context.Products.Include(p=> p.Orders).ThenInclude(c=> c.Customer).ToListAsync();
