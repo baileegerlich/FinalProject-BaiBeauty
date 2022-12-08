dba7cfdfcfc6f4f0dd4758a63ba82f7afd79d60b
Bailee Gerlich <bngerlich2@buffs.wtamu.edu>
2022-12-08 15:13:17 -0600

    Newest Push

--- Pages/PagesProducts/Details.cshtml.cs
+++ Pages/PagesProducts/Details.cshtml.cs
@@ -33,26 +33,20 @@ namespace FinalProject_BaiBeauty.PagesProducts
         public int CustomerIdToAdd {get;set;}
         //List Customer/Dropdown
         public List<Customer>AllCustomers {get;set;} = default!;
+        [BindProperty]
+        [Required]
         public SelectList CustomerDropDown{get;set;} = default!;
 
         public async Task<IActionResult> OnGetAsync(int? id)
-        {   
-            //Add Customer
-            _logger.LogWarning($"OnPost: ProductID {id}, ADD Customer {CustomerIdToAdd}");
-            if (CustomerIdToAdd == 0)
-            {
-                ModelState.AddModelError("CourseIdToAdd", "This field is a required field.");
-                return Page();
-            }
-            if (id == null)
+        { 
+             if (id == null || _context.Products == null)
             {
                 return NotFound();
             }
 
-            var product = await _context.Products.Include(o=> o.Orders).ThenInclude(c=>c.Customer).FirstOrDefaultAsync(m => m.ProductID == id);
+            var product = await _context.Products.Include(s => s.Orders!).ThenInclude(sc => sc.Customer).FirstOrDefaultAsync(m => m.ProductID == id);
             AllCustomers = await _context.Customers.ToListAsync();
-            CustomerDropDown = new SelectList(AllCustomers, "CustomerID","firstName","lastName");
-
+            CustomerDropDown = new SelectList(AllCustomers, "CustomerID", "firstName","lastName");
             if (product == null)
             {
                 return NotFound();
@@ -61,18 +55,7 @@ namespace FinalProject_BaiBeauty.PagesProducts
             {
                 Product = product;
             }
-             if (!_context.Order.Any(sc => sc.CustomerID == CustomerIdToAdd && sc.ProductID == id.Value))
-            {
-                Order customerToAdd = new Order { ProductID = id.Value, CustomerID = CustomerIdToAdd};
-                _context.Add(CustomerIdToAdd);
-                _context.SaveChanges();
-            }
-            else
-            {
-                _logger.LogWarning("Customer is already in this order");
-            }
-
-            return RedirectToPage(new {id = id});
+            return Page();
         }
         // OnPost for Delete Customer
          public async Task<IActionResult> OnPostDeleteCustomerAsync(int? id)
@@ -108,7 +91,45 @@ namespace FinalProject_BaiBeauty.PagesProducts
 
             return RedirectToPage(new {id = id});
         }
-       
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            _logger.LogWarning($"OnPost: CustomerID {id}, ADD Customer {CustomerIdToAdd} {}");
+            if (CustomerIdToAdd == 0)
+            {
+                ModelState.AddModelError("CustomerIdToAdd", "This field is a required field.");
+                return Page();
+            }
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products.Include(s => s.Orders!).ThenInclude(sc => sc.Customer).FirstOrDefaultAsync(m => m.ProductID == id);            
+            AllCustomers = await _context.Customers.ToListAsync();
+            CustomerDropDown = new SelectList(AllCustomers, "CustomerID", "firstName","lastName");
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Product = product;
+            }
+
+            if (!_context.Order.Any(sc => sc.CustomerID == CustomerIdToAdd && sc.ProductID == id.Value))
+            {
+                Order customerToAdd = new Order { ProductID = id.Value, CustomerID = CustomerIdToAdd};
+                _context.Add(customerToAdd);
+                _context.SaveChanges();
+            }
+            else
+            {
+                _logger.LogWarning("Customer is already in this order.");
+            }
+
+            return RedirectToPage(new {id = id});
+        }
         
     }
 }
